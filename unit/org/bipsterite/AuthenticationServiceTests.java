e740cd16c50144371e8d0e99dc4679c03b90949d
Jim <openbip@gmail.com>
2011-02-27 00:17:24 -0500

    Added target for unit-testing

--- unit/org/bipsterite/AuthenticationServiceTests.java
+++ unit/org/bipsterite/AuthenticationServiceTests.java
@@ -3,10 +3,17 @@
  */
 package org.bipsterite;
 
+import org.bipsterite.user.AuthenticationService;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author bippy
- *
  */
 public class AuthenticationServiceTests {
-
+    @Test
+    public void testBasic() {
+        AuthenticationService service = new AuthenticationService();
+        Assert.assertTrue(service.authenticateUser("bippy", "bippy"));
+    }
 }
